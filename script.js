a3098393fd6d5738474c8c025856e6be22e94a0e
Kanchanprajapat <141110309+Kanchanprajapat@users.noreply.github.com>
2024-06-26 12:31:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function makeid(l) {
   // write your code here
+	 let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.l;
+
+  for (let i = 0; i < l; i++) {
+    const randomChar = characters.charAt(Math.floor(Math.random() * charactersLength));
+    result += randomChar;
+  }
+  return result;
 }
 
 // Do not change the code below.
